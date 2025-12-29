610cafb089e5596a8c489760772886c736f5e5cd
Eatham532 <78714349+Eatham532@users.noreply.github.com>
2025-12-29 00:36:00 -0500

    feat: new instance icons
    Signed-off-by: Eatham532 <78714349+Eatham532@users.noreply.github.com>

--- vesta-launcher/src/utils/instances.ts
+++ vesta-launcher/src/utils/instances.ts
@@ -2,11 +2,19 @@ import { invoke } from "@tauri-apps/api/core";
 import { listen } from "@tauri-apps/api/event";
 import PlaceholderImage1 from "@assets/placeholder-images/placeholder-image1.jpg";
 import PlaceholderImage2 from "@assets/placeholder-images/placeholder-image2.png";
+import PlaceholderImage3 from "@assets/placeholder-images/placeholder-image3.png";
+import PlacehodlerImage4 from "@assets/placeholder-images/placeholder-image4.png";
+import PlaceholderImage5 from "@assets/placeholder-images/placeholder-image5.png";
+import PlaceholderImage6 from "@assets/placeholder-images/placeholder-image6.png";
 
 // Default icons for instances
 export const DEFAULT_ICONS = [
 	PlaceholderImage1,
 	PlaceholderImage2,
+	PlaceholderImage3,
+	PlacehodlerImage4,
+	PlaceholderImage5,
+	PlaceholderImage6,
 	"linear-gradient(135deg, #FF6B6B 0%, #EE5D5D 100%)",
 	"linear-gradient(135deg, #4FACFE 0%, #00F2FE 100%)",
 	"linear-gradient(135deg, #43E97B 0%, #38F9D7 100%)",
